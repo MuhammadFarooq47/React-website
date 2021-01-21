afb96f4fc2f8962ad9cdd7a4f5754ab0161fbd2d
Muhammd Farooq <m.farooq.h7@gmail.com>
2021-01-21 17:14:26 +0500

    Updates

---
+++ precache-manifest.56c7567869a9019663a2d14f1798cb77.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3948109caa823303245d41e8b58841",
+    "url": "/React-website/index.html"
+  },
+  {
+    "revision": "046052fc7d447d1f65ce",
+    "url": "/React-website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "55524d15a7314e70aac0",
+    "url": "/React-website/static/css/main.f83dcde0.chunk.css"
+  },
+  {
+    "revision": "046052fc7d447d1f65ce",
+    "url": "/React-website/static/js/2.b134e863.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React-website/static/js/2.b134e863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55524d15a7314e70aac0",
+    "url": "/React-website/static/js/main.6e8fa130.chunk.js"
+  },
+  {
+    "revision": "02b71d310729e812f1f5",
+    "url": "/React-website/static/js/runtime-main.04671cb5.js"
+  },
+  {
+    "revision": "8b019790725eaa4ef47185a5be0bf0cc",
+    "url": "/React-website/static/media/about1.8b019790.png"
+  },
+  {
+    "revision": "c4db786a9ad2b8ea366dd620f45fba83",
+    "url": "/React-website/static/media/analysis.c4db786a.jpg"
+  },
+  {
+    "revision": "cb04a9874a1fefdd7f13aabf8d58b674",
+    "url": "/React-website/static/media/asset.cb04a987.svg"
+  },
+  {
+    "revision": "33ecfc07a070633535ef4d126540987c",
+    "url": "/React-website/static/media/customize.33ecfc07.svg"
+  },
+  {
+    "revision": "cb772c85a46d61498a47f90534a98b25",
+    "url": "/React-website/static/media/design.cb772c85.jpg"
+  },
+  {
+    "revision": "4e799ebdd1df61ab5d6821750ee3140c",
+    "url": "/React-website/static/media/designteam.4e799ebd.svg"
+  },
+  {
+    "revision": "309cedad36d85e60d7af3c0675acb859",
+    "url": "/React-website/static/media/development.309cedad.jpg"
+  },
+  {
+    "revision": "1e668e0df9d6c84810ce54e294a38507",
+    "url": "/React-website/static/media/home.1e668e0d.png"
+  }
+]);
